25a73fbf34026af077755fac6197a616e411041c
Tim Kung <revolution.tdk@gmail.com>
2015-10-02 11:37:52 +1300

    adding basic map.js to layout map level and accompanying basic room pngs

---
+++ map.js
@@ -0,0 +1,54 @@
+var Map = [
+	{
+		"xPos": 96,
+		"yPos": 0,
+		"url": "./assets/room1.png"
+	},
+	{
+		"xPos": 32,
+		"yPos": 96,
+		"url": "./assets/room2.png"
+	},
+	{
+		"xPos": 0,
+		"yPos": 128,
+		"url": "./assets/room3.png"
+	},
+	{
+		"xPos": 32,
+		"yPos": 256,
+		"url": "./assets/room4.png"
+	},
+	{
+		"xPos": 96,
+		"yPos": 224,
+		"url": "./assets/room5.png"
+	},
+	{
+		"xPos": 160,
+		"yPos": 160,
+		"url": "./assets/room6.png"
+	},
+	{
+		"xPos": 224,
+		"yPos": 192,
+		"url": "./assets/room7.png"
+	},
+	{
+		"xPos": 224,
+		"yPos": 256,
+		"url": "./assets/room8.png"
+	},
+	{
+		"xPos": 160,
+		"yPos": 320,
+		"url": "./assets/room9.png"
+	},
+	{
+		"xPos": 96,
+		"yPos": 352,
+		"url": "./assets/room10.png"
+	}
+];
+
+export default Map;
