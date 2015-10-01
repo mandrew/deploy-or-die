d3e727e3a05683492507c3e6538b5cf77d62f71e
Tim Kung <revolution.tdk@gmail.com>
2015-10-02 11:53:41 +1300

    adding width and heights to rooms on map.js and renaming xpos and ypos to left and top

--- map.js
+++ map.js
@@ -1,53 +1,73 @@
 var Map = [
 	{
-		"xPos": 96,
-		"yPos": 0,
-		"url": "./assets/room1.png"
+		"left": 96,
+		"top": 0,
+		"url": "./assets/room1.png",
+		"width": 96,
+		"height": 192
 	},
 	{
-		"xPos": 32,
-		"yPos": 96,
-		"url": "./assets/room2.png"
+		"left": 32,
+		"top": 96,
+		"url": "./assets/room2.png",
+		"width": 64,
+		"height": 128
 	},
 	{
-		"xPos": 0,
-		"yPos": 128,
-		"url": "./assets/room3.png"
+		"left": 0,
+		"top": 128,
+		"url": "./assets/room3.png",
+		"width": 32,
+		"height": 224
 	},
 	{
-		"xPos": 32,
-		"yPos": 256,
-		"url": "./assets/room4.png"
+		"left": 32,
+		"top": 256,
+		"url": "./assets/room4.png",
+		"width": 64,
+		"height": 96
 	},
 	{
-		"xPos": 96,
-		"yPos": 224,
-		"url": "./assets/room5.png"
+		"left": 96,
+		"top": 224,
+		"url": "./assets/room5.png",
+		"width": 64,
+		"height": 128
 	},
 	{
-		"xPos": 160,
-		"yPos": 160,
-		"url": "./assets/room6.png"
+		"left": 160,
+		"top": 160,
+		"url": "./assets/room6.png",
+		"width": 160,
+		"height": 128
 	},
 	{
-		"xPos": 224,
-		"yPos": 192,
-		"url": "./assets/room7.png"
+		"left": 224,
+		"top": 192,
+		"url": "./assets/room7.png",
+		"width": 96,
+		"height": 96
 	},
 	{
-		"xPos": 224,
-		"yPos": 256,
-		"url": "./assets/room8.png"
+		"left": 224,
+		"top": 256,
+		"url": "./assets/room8.png",
+		"width": 96,
+		"height": 160
 	},
 	{
-		"xPos": 160,
-		"yPos": 320,
-		"url": "./assets/room9.png"
+		"left": 160,
+		"top": 320,
+		"url": "./assets/room9.png",
+		"width": 64,
+		"height": 128
 	},
 	{
-		"xPos": 96,
-		"yPos": 352,
-		"url": "./assets/room10.png"
+		"left": 96,
+		"top": 352,
+		"url": "./assets/room10.png",
+		"width": 96,
+		"height": 128
 	}
 ];
 
